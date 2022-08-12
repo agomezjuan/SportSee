76fb1d38c44a58bd3dcd89637e6cc66e4f3dc5c4
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-08-12 18:52:48 +0200

    Button component and styles

---
+++ src/components/Menu/Button.jsx
@@ -0,0 +1,13 @@
+import './index.sass'
+
+const Button = ({props}) => {
+    const {activity, icon} = props
+    
+  return (
+    <div className='button'>
+        <img src={icon} alt={activity} />
+    </div>
+  )
+}
+
+export default Button
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,28 @@
+import Button from './Button'
+import meditation from '../../assets/meditation.svg'
+import swimming from '../../assets/swimming.svg'
+import biking from '../../assets/biking.svg'
+import lifting from '../../assets/lifting.svg'
 import './index.sass'
-
+console.log(meditation);
+console.log(swimming);
+console.log(biking);
+console.log(lifting);
 const Menu = () => {
+
+  const buttons = [
+    {activity: "Meditation", icon: meditation},
+    {activity: "Swimming", icon: swimming},
+    {activity: "Biking", icon: biking},
+    {activity: "Lifting", icon: lifting},
+  ]
+
   return (
     <aside className='left-menu'>
-        <p>Hola</p>
+      <div className="menu-options">
+        {buttons.map((button, index) => {return <Button props={button} key={index} />})}
+      </div>
+        <p>Copiryght, SportSee 2020</p>
     </aside>
   )
 }
