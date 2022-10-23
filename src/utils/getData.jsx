1bab169666c3b77c0695ebdbb6e02c938f052741
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-23 03:20:01 +0200

    feat: api calls

--- src/utils/getData.jsx
+++ src/utils/getData.jsx
@@ -1,34 +1,45 @@
 import axios from "axios";
-import { BrowserRouter, Navigate } from "react-router-dom";
-const baseURL = "http://localhost:3000/user/";
 
 async function getUserInfo(id) {
+  const baseURL = "http://localhost:3000/user";
   try {
-    const reqUserInfo = axios.get(`${baseURL}${id}`);
-    const reqUserPerformance = axios.get(`${baseURL}${id}/performance`);
-    const reqUserAverage = axios.get(`${baseURL}${id}/average-sessions`);
-    const reqUserActivity = axios.get(`${baseURL}${id}/activity`);
+    const reqUserInfo = axios.get(`${baseURL}/${id}`);
+    const reqUserPerformance = axios.get(`${baseURL}/${id}/performance`);
+    const reqUserAverage = axios.get(`${baseURL}/${id}/average-sessions`);
+    const reqUserActivity = axios.get(`${baseURL}/${id}/activity`);
 
-    const [userInfo, userPerformance, userAverage, userActivity] =
-      await axios.all([
-        reqUserInfo,
-        reqUserPerformance,
-        reqUserAverage,
-        reqUserActivity,
-      ]);
+    const [userInfo, userPerformance, userAverage, userActivity] = await axios.all([
+      reqUserInfo,
+      reqUserPerformance,
+      reqUserAverage,
+      reqUserActivity
+    ]);
 
     const data = userInfo.data.data;
+
     const firstName = data.userInfos.firstName;
     const lastName = data.userInfos.lastName;
     const age = data.userInfos.age;
     const todayScore = data.todayScore;
     const keyData = data.keyData;
-    const activity = userActivity.data.data.sessions;
+    const activity = userActivity.data.data.sessions.map(session => {
+      return {
+        day: parseInt(session.day.slice(-2)),
+        kilogram: session.kilogram,
+        calories: session.calories
+      };
+    });
     const average = userAverage.data.data.sessions;
+    console.log("activity", activity);
+    console.log("average", average);
 
-    const performance = userPerformance.data.data.data.map();
-    console.log(performance);
-
+    const performance = userPerformance.data.data.data.map((item, index) => {
+      return {
+        kind: Object.values(userPerformance.data.data.kind)[index],
+        value: item.value
+      };
+    });
+    console.log("Performance", performance);
     return {
       firstName,
       lastName,
@@ -37,16 +48,11 @@ async function getUserInfo(id) {
       keyData,
       activity,
       average,
+      performance
     };
   } catch (error) {
-    if (error.request.response) {
-      console.log(error.request.response);
-      return (
-        <BrowserRouter>
-          <Navigate replace to="/user/not-found" />
-        </BrowserRouter>
-      );
-    }
+    console.log(error);
+    window.location.replace("/user/not-found");
   }
 }
 
